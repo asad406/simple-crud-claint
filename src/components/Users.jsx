a93a23e1103e5033e9ae7d002393c9c4c885e52a
asad406 <asadl76@gmail.com>
2023-05-09 11:50:13 +0600

    Data delete from database functionality added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,13 +3,29 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const users = useLoaderData()
+    const users = useLoaderData();
+
+    const handleDelete = (_id) => {
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if(data.deletedCount > 0){
+                    alert('Delete successfully')
+                }
+            });
+    };
     return (
         <div>
             <h2>Length of Users : {users.length}</h2>
-            {
-                users.map(user => <p key={user._id}>name:{user.name}, email : {user.email}</p>)
-            }
+            {users.map((user) => (
+                <p key={user._id}>
+                    name:{user.name}, email : {user.email}
+                    <button onClick={() => handleDelete(user._id)}>X</button>
+                </p>
+            ))}
         </div>
     );
 };
